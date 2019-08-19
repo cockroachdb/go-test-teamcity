3e4c912a70d6bf3e5c563b58c4466cd7919516bf
David Taylor <tinystatemachine@gmail.com>
2019-08-19 04:15:42 +0000

    fix tests to handle verbose vs non-verbose output

--- main_test.go
+++ main_test.go
@@ -22,54 +22,61 @@ var (
 
 func TestProcessReader(t *testing.T) {
 	for _, json := range []bool{false, true} {
-		t.Run(fmt.Sprintf("json=%t", json), func(t *testing.T) {
-			sep := ""
-			if json {
-				sep = ".json"
-			}
+		for _, verbose = range []bool{false, true} {
+			t.Run(fmt.Sprintf("json=%t,v=%t", json, verbose), func(t *testing.T) {
+				sep := ""
+				if json {
+					sep = ".json"
+				}
 
-			files, err := ioutil.ReadDir(inDir + sep)
-			if err != nil {
-				t.Error(err)
-			}
-			for _, file := range files {
-				t.Run(file.Name(), func(t *testing.T) {
-					inpath := filepath.Join(inDir+sep, file.Name())
-					f, err := os.Open(inpath)
-					if err != nil {
-						t.Error(err)
-					}
-					in := bufio.NewReader(f)
+				files, err := ioutil.ReadDir(inDir + sep)
+				if err != nil {
+					t.Error(err)
+				}
+				for _, file := range files {
+					t.Run(file.Name(), func(t *testing.T) {
+						inpath := filepath.Join(inDir+sep, file.Name())
+						f, err := os.Open(inpath)
+						if err != nil {
+							t.Error(err)
+						}
+						in := bufio.NewReader(f)
 
-					out := &bytes.Buffer{}
-					if json {
-						processJSON(in, out)
-					} else {
-						processReader(in, out)
-					}
-					actual := out.String()
-					actual = timestampRegexp.ReplaceAllString(actual, timestampReplacement)
+						out := &bytes.Buffer{}
+						if json {
+							processJSON(in, out)
+						} else {
+							processReader(in, out)
+						}
+						actual := out.String()
+						actual = timestampRegexp.ReplaceAllString(actual, timestampReplacement)
 
-					outpath := filepath.Join(outDir+sep, file.Name())
-					t.Logf("input: %s", inpath)
-					t.Logf("output: %s", outpath)
-					expectedBytes, err := ioutil.ReadFile(outpath)
-					if err != nil {
-						t.Error(err)
-					}
-					expected := string(expectedBytes)
-					expected = timestampRegexp.ReplaceAllString(expected, timestampReplacement)
+						file := file.Name()
+						if !verbose {
+							file += "_quiet"
+						}
+						outpath := filepath.Join(outDir+sep, file)
 
-					const rewriteOutput = true
-					if rewriteOutput {
-						_ = ioutil.WriteFile(outpath, []byte(actual), 0644)
-					}
+						t.Logf("input: %s", inpath)
+						t.Logf("output: %s", outpath)
+						expectedBytes, err := ioutil.ReadFile(outpath)
+						if err != nil {
+							t.Error(err)
+						}
+						expected := string(expectedBytes)
+						expected = timestampRegexp.ReplaceAllString(expected, timestampReplacement)
 
-					if strings.Compare(expected, actual) != 0 {
-						t.Errorf("expected:\n\n%s\nbut got:\n\n%s\n", expected, actual)
-					}
-				})
-			}
-		})
+						const rewriteOutput = true
+						if rewriteOutput {
+							_ = ioutil.WriteFile(outpath, []byte(actual), 0644)
+						}
+
+						if strings.Compare(expected, actual) != 0 {
+							t.Errorf("expected:\n\n%s\nbut got:\n\n%s\n", expected, actual)
+						}
+					})
+				}
+			})
+		}
 	}
 }
